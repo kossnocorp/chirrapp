a38c1fa6b660f1329dc7788efc5e8c60ad01e405
Sasha Koss <koss@nocorp.me>
2017-08-30 23:57:20 +0400

    Upgrade Clap widget

--- app/UI/Done/ClapBanner/index.jsx
+++ app/UI/Done/ClapBanner/index.jsx
@@ -4,7 +4,7 @@ import Banner from 'app/UI/_lib/Banner'
 export default function ClapBanner() {
   return (
     <Banner id="clap">
-      <div id="clap-widget">
+      <div id="clap-widget" data-key="-KscuHHA1socHVlyacdZ">
         <script async src="https://clap.tips/widget.js" async />
       </div>
     </Banner>
