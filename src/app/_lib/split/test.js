b0af2de77e2fc4003c8ff25b1a05d92089869468
Topciu Maxim <maximtop@gmail.com>
2017-12-09 18:17:06 +0300

    remove additional scripts from package.json

--- src/app/_lib/split/test.js
+++ src/app/_lib/split/test.js
@@ -5,7 +5,7 @@ test(t => {
   t.deepEqual(split('Short tweet.'), ['Short tweet.'])
   const url =
     'http://example.com/9531AA08FB25F8146A0472EE7E43623010CACC7E8BFB7C992229A39B9210248E795A84197EE01B9D50B40889BC5689E930A8839DB3D43010E887DDEEE643CCDCEE5EDCBD6A866480FFAC850D7EDE367B859E70F0D7E5E5E28B78CE6E4D5D35748310898A8DA4EC8B82A2E7AF72596F64E4742C22273FC8865662ED7F64AC8D01F3FFD442BB64AD6B9FD79B523BB586252C1D5A4CAE50548C56CBBDA498A2BEF11927B4D9CEE85854A0DC3DBAC0EE46659A0ADEBB7D5678E25E13D6AD2AADC625B4907CF45E3B0EC2602F03179E3FA3A27D8BEF198E456B0F5CBBFB9DB182C2499A359D3A8A92771918906EBC85B0CD3215A861F69D527135B28A362117CFF63F'
-  
+
   t.deepEqual(split(`Short tweet with extremly long URL: ${url}`), [
     `Short tweet with extremly long URL: ${url}`
   ])
@@ -29,7 +29,7 @@ test(t => {
     split('It[...]allows[...]to[...]split[...]tweets[...]manually!'),
     ['It', 'allows', 'to', 'split', 'tweets', 'manually!']
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit ultricies. Nullam ante risus,[...]vehicula at arcu eleifend, tempus condimentum lectus.'
@@ -39,7 +39,7 @@ test(t => {
       'vehicula at arcu eleifend, tempus condimentum lectus.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit[...]ultricies. Nullam ante risus, vehicula at arcu eleifend tempus condimentum lectus.'
@@ -49,7 +49,7 @@ test(t => {
       'ultricies. Nullam ante risus, vehicula at arcu eleifend tempus condimentum lectus.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit ultricies. Nullam ante risus, vehicula at[...]arcu.'
@@ -59,7 +59,7 @@ test(t => {
       'arcu.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit ultricies. Nullam ante risus, vehicula at arcu.[...]'
@@ -68,7 +68,7 @@ test(t => {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit ultricies. Nullam ante risus, vehicula at arcu.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique nunc quis blandit ultricies. Nullam[...]ante risus, vehicula at.'
@@ -90,7 +90,7 @@ test(t => {
       '…nullam ante risus, vehicula at arcu eleifend, tempus condimentum lectus.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum. Dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus. Vehicula at arcu eleifend, tempus condimentum lectus proin lacinia dolor elit, nec laoreet magna ultrices vel maecenas scelerisque tortor massa, non faucibus dui consectetur sed Lorem ipsum Dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus Vehicula at arcu eleifend, tempus condimentum lectus proin lacinia dolor elit, nec laoreet magna ultrices vel maecenas scelerisque tortor massa, non faucibus dui consectetur sed.'
@@ -101,7 +101,7 @@ test(t => {
       '…scelerisque tortor massa, non faucibus dui consectetur sed.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'LoremxipsumxdolorxsitxametxxconsecteturxadipiscingxelitxxnullaxtristiquexnuncxquisxblanditxultriciesxxnullamxantexrisusxxvehiculaxatxarcuxeleifendxxtempusxcondimentumxlectusxLoremxipsumxdolorxsitxametxxconsecteturxadipiscingxelitxxnullaxtristiquexnuncxquisxblanditxultriciesxxnullamxantexrisusxxvehiculaxatxarcuxeleifendxxtempusxcondimentumxlectusx'
@@ -151,7 +151,7 @@ test('numbering', t => {
       "3/ After a successful release, I realized how stupid I was and promised myself to never repeat the mistake. It's been alright until I decided to finally release v2.0.0... 8 months ago. I mean, yeah, v2.0.0 is coming really soon."
     ]
   )
-  
+
   t.deepEqual(
     split(
       `test test test test test[...] test test test test test.[...] test test test test test test test test test test test test test…
@@ -165,7 +165,7 @@ test('numbering', t => {
       '3/ test test test test test test test test test test test test test… \n \n …test test'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus, vehicula at arcu eleifend, tempus condimentum lectus Lorem ipsum dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus, vehicula at arcu eleifend, tempus condimentum lectus.',
@@ -176,7 +176,7 @@ test('numbering', t => {
       '2/ …nullam ante risus, vehicula at arcu eleifend, tempus condimentum lectus.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'Lorem ipsum dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus vehicula at arcu eleifend, tempus condimentum lectus proin lacinia dolor elit, nec laoreet magna ultrices vel maecenas scelerisque tortor massa, non faucib dui consectetur sed Lorem ipsum dolor sit amet, consectetur adipiscing elitx nulla tristique nunc quis blandit ultriciesx nullam ante risus vehicula at arcu eleifend, tempus condimentum lectus proin lacinia dolor elit, nec laoreet magna ultrices vel maecenas scelerisque tortor massa, non faucib dui consectetur sed.',
@@ -188,7 +188,7 @@ test('numbering', t => {
       '3/ …tortor massa, non faucib dui consectetur sed.'
     ]
   )
-  
+
   t.deepEqual(
     split(
       'It[...]allows[...]to[...]Test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test Test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test[...]split[...]tweets[...]manually!',
@@ -207,7 +207,7 @@ test('numbering', t => {
       '8/ manually!'
     ]
   )
-  
+
   t.deepEqual(
     split(
       `According to @johnredwood responding to Tony Blair @InstituteGC According to @johnredwood responding to Tony Blair @InstituteGC : "Mr Blair's fantasy re negotiation of the UK’s deal with the EU ignores Mr Cameron’s long attempt to negotiate just such a change to the EU benefits and migration policies without any success. Mr Blair's fantasy re negotiation of the UK’s deal with the EU ignores Mr Cameron’s long attempt to negotiate just such a change to the EU benefits and migration policies without any success." The manifesto on which he and @David_Cameron were elected in 2015 stated that The manifesto on which he and @David_Cameron were elected in 2015 stated that: "We will negotiate new rules with the EU, so that people will have to be earning here for a number of years before they can claim benefits We will negotiate new rules with the EU, so that people will have to be earning here for a number of years before they can claim benefits". That was agreed in the February 2016 European Council "settlement" decision That was agreed in the February 2016 European Council "settlement" decision. The UK would have been allowed to "to limit the access of newly arriving EU workers to non-contributory in-work benefits for a total period of up to four years from the commencement of employment The UK would have been allowed to "to limit the access of newly arriving EU workers to non-contributory in-work benefits for a total period of up to four years from the commencement of employment"`,
